0ba1f9dd8e02dbe31983ddc4b25b41eae73ebf65
Sudarsini2156 <sudarsini2156@gmail.com>
2026-03-14 11:30:45 +0530

    fix server log for assignment

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
-def add(a, b):
-    return a + b
+from flask import Flask
 
+app = Flask(__name__)
 
-def subtract(a, b):
-    return a - b
+@app.route("/")
+def home():
+    return "SERVICERUNNING"
+
+if __name__ == "__main__":
+    print("SERVICERUNNING")
+    app.run(host="0.0.0.0", port=8000)
